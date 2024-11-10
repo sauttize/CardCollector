2839e14411aa6d8f022547deba59d165e1d624f0
sauttize <ntejera@gmail.com>
2024-11-10 01:29:05 -0300

    Added wallet and profile info

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
 import './styles/Header.css'
+import MoneyIcon from '/icons/money.svg'
 
 function Header() {
   return (
-    <div className="header dark-mode"></div>
+    <div className="header dark-mode">
+      <div className="wallet-info">
+        <img src={MoneyIcon}></img>
+        <div className="money-amount">0</div>
+      </div>
+      <div className="profile-info">
+        <div className="name-lvl">
+          <div className="name">username</div>
+          <div className="lvl">Lvl</div>
+        </div>
+        <div className="pp-container">
+          <img></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
